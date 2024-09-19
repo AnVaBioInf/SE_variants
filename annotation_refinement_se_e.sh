5b32824a2f92450e39055edc88af24da834582eb
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-09-19 13:44:34 +0300

    Update annotation_refinement_se_e.sh

--- annotation_refinement_se_e.sh
+++ annotation_refinement_se_e.sh
@@ -9,7 +9,17 @@ awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA.bed > ENCFF900YFA_classi
 
 # excluding SE from enhancer file
 ~/programs/bedtools2/bin/bedtools intersect -v -f 1.0 -a ~/project/enhancers/ENCFF900YFA_classified.bed \
--b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > ENCFF900YFA_classified_noSE.bed
+-b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > ~/project/enhancers/ENCFF900YFA_classified_noSE.bed
+
+# statistics
+# total number of high H3K27ac ENCODE predicted enhancer elements in SE
+cat ~/project/se/se_encode_e_classified.bed | wc -l
+# 31726
+
+# total number of typical enhancers (not intersecting with SE)
+cat ~/project/enhancers/ENCFF900YFA_classified_noSE.bed | wc -l
+# 117966
+
 
 # check -f!!!
 # make sure, that all enhancers intersecting SE on whatever number of bases are not included in 
