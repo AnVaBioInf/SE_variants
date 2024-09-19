19298cac4ed81cd5803fa7855ad27cd1716bae08
AnVaBioInf <96749066+AnVaBioInf@users.noreply.github.com>
2024-09-19 23:59:43 +0300

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mkdir ~/project/variants/vcf_1000_genomes_filtered
-mkdir ~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_se
-mkdir ~/project/variants/vcf_1000_genomes_filteredvcf_1000_genomes_filtered_e_no_se
 
 cd ~/project/variants/vcf_1000_genomes/
 vcf_files=(*)
@@ -14,14 +12,6 @@ for i in "${vcf_files[@]}"; \
 do \
 ~/programs/bedtools2/bin/bedtools intersect -wa -wb \
 -a ~/project/variants/vcf_1000_genomes/$i \
--b ~/project/se/se_encode_e_classified.bed > ~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_se/${i%.vcf*}_filtered_se.vcf; \
-done;
-
-# leaving only mutations in typical enhancers (taken from ENCODE) (SE enhancer elements were excluded)
-for i in "${vcf_files[@]}"; \
-do \
-~/programs/bedtools2/bin/bedtools intersect  -wa -wb \
--a ~/project/variants/vcf_1000_genomes/$i \
--b ~/project/enhancers/ENCFF900YFA_classified_noSE.bed > \
-~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_e_no_se/${i%.vcf*}_filtered_e_no_se.vcf.gz; \
+-b ~/project/enhancers_refined_se_e.bed > \
+~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_se/${i%.vcf*}_filtered_se.vcf; \
 done;
